3e140f8f7be19a9a9c298c3aeb6ac741385702ee
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-26 14:50:20 -0600

    Refactor: service created and controller refactored, facade up next

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,8 @@
 class SearchController < ApplicationController
   def index
-    nation = params["nation"]
-    
-    conn = Faraday.new(url: "https://last-airbender-api.fly.dev")
-    
-    response = conn.get("/api/v1/characters?affiliation=#{nation}&perPage=100")
+    @title = params[:nation].gsub("+", " ").titleize
 
-    @title = nation.gsub("+", " ").titleize
-    @parsed = JSON.parse(response.body, symbolize_names: true)
+    @parsed = MembersService.new.get_members(params["nation"])
 
     @members = @parsed[0..24].map do |member|
       Member.new(member)
---
+++ app/services/members_service.rb
@@ -0,0 +1,14 @@
+class MembersService
+  def get_members(nation)
+    get_url("/api/v1/characters?affiliation=#{nation}&perPage=100")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://last-airbender-api.fly.dev")
+  end
+end
