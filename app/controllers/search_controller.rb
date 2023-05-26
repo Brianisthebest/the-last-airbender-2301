87f4b084e6d57aba19a6427519538cc26dcc6f55
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-05-26 13:35:18 -0600

    Feat: need for a poros, and got total count of members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,15 @@ class SearchController < ApplicationController
   def index
     nation = params["nation"]
     
-    conn = Faraday.new(url: "https://last-airbender-api.fly.dev") do |faraday|
-      params[:perPage] = 100
-    end
-    response = conn.get("/api/v1/characters?affiliation=#{nation}")
+    conn = Faraday.new(url: "https://last-airbender-api.fly.dev")
+    
+    response = conn.get("/api/v1/characters?affiliation=#{nation}&perPage=100")
 
-    require 'pry'; binding.pryq
+    parsed = JSON.parse(response.body, symbolize_names: true)
+require 'pry'; binding.pry
+# total count of members is 97
+    parsed.map do |member|
+      Member.new(member)
+    end
   end
 end
